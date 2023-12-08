ed173ffc58853745fea55968208863c27fedea28
Bonadventurov <149107799+Bonadventurov@users.noreply.github.com>
2023-12-08 13:28:46 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -55,9 +55,31 @@ void showEl_matrix(const int* ar, int size, int col, char del = ' '){
 	std::cout << std::endl;
 }
 
+void SelSort(int size; int ar[size]){
+  3     for(int i = 0; i < size; i++){
+  4         int max = ar[0];
+  5         for(int j = 0; j < size; j++){
+  6             if(max - ar[j] < 0){
+  7                 max = ar[j];
+  8             }
+  9         }
+ 10                 int temp = ar[j];
+ 11                 ar[j] = ar[0];
+ 12                 ar[0] = temp;
+ 13                 int br[size - 1];
+ 14                 for(int q = 1; q < size - 1; q++){
+ 15                     br[i] = ar[q];
+ 16                 }
+ 17     }
+ 18     std::cout <<
+ 19 }
+ 
 
-
-int main(){
+int main(int c, char** agv){
+	std::cout << "--------\ntest test\n------" << std::endl;;
+	for (int i = 0; i < c; i++) {
+		std::cout << agv[i] << std::endl;
+	}
     return 0;
 }
 
